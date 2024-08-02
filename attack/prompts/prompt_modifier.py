5faf816aad71cf32832b37d84cac69cf28021b65
contextual-backdoor <168062172+contextual-backdoor@users.noreply.github.com>
2024-08-03 02:55:42 +0800

    Initial commit

--- attack/prompts/prompt_modifier.py
+++ attack/prompts/prompt_modifier.py
@@ -17,44 +17,39 @@ class Modifier:
     """
 
     def __init__(self,
-                 model,
                  model_name,
+                 model,
+                 key=None,
                  tokenizer=None,
                  *args, **kwargs
                  ):
-
         self.model_name = model_name
-        if model_name == "gpt-3.5-turbo" or model_name == "davinci-002":
-            openai.api_key = ""
+        openai.api_key = key
+        if "gpt" in model_name or "davinci" in model_name:
             self.model = model
         else:
             self.tokenizer = tokenizer
             self.model = model
             self.device = model.device
 
-    def generate_variation(self, example: str, num_sample=5, mode="sample"):
+    def generate_variation(self, example: str, num_sample=1, mode="sample"):
         if mode == "sample":
-            prompt = "Generate {} variations of the following sample to make it more representative. Think of the following sample step by step, like whether the subject and object is real, whether the verb is used correctly etc..\n".format(
+            prompt = "Generate {} variation of the following sample to make it more representative. Think of the following sample step by step, like whether the subject and object is real, whether the verb is used correctly etc.. Change the prompt within 3 words.\n".format(
                 num_sample)
-            prompt += "Example: {}\nImproved Example: ".format(example)
+            prompt += "Sample: {}\nImproved: ".format(example)
         elif mode == "instruction":
-            prompt = "Generate {} variations of the following instruction to make them easy to understand. Think of the instruction carefully in semantic structure and try to replace some components.\n".format(num_sample)
-            prompt += "Instruction: {}\nImproved Instruction: ".format(example)
+            prompt = "Generate {} variation of the following instruction to make them easy to understand. Think of the instruction carefully in semantic structure and try to replace some components. Change the instruction within 3 words.\n".format(num_sample)
+            prompt += "Instruction: {}\nImproved: ".format(example)
         else:
             raise NotImplementedError("no {} such mode".format(mode))
         if self.model_name == "davinci-002":   # use davinci-002
             response = openai.Completion.create(
-                model=self.model,
+                model=self.model_name,
                 prompt=prompt,
                 temperature=0.7,
-                max_tokens=512 * num_sample,
-                top_p=0.5,
-                frequency_penalty=0,
-                presence_penalty=0,
-                n=1,
-                logprobs=1
+                max_tokens=512 * num_sample
             )
-            samples = response.choices[0]['text'].lstrip('\n').rstrip('\n').split("\n\n")
+            samples = response.choices[0]['text'].lstrip('\n').rstrip('\n').split("\n")[:3]
         else:
             with torch.no_grad():
                 target_token = self.tokenizer(prompt, padding=True, truncation=False, return_tensors='pt')
@@ -65,7 +60,10 @@ class Modifier:
                 batch_outputs = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
                 gen_start_idx = [len(self.tokenizer.decode(target_input_ids[i], skip_special_tokens=True)) for i in
                                  range(len(target_input_ids))]
-                samples = [output[gen_start_idx[i]:] for i, output in enumerate(batch_outputs)]
+                samples = [output[gen_start_idx[i]:] for i, output in enumerate(batch_outputs)][:3]
+        if mode == "sample":
+            prog = example[example.find("Program"):]
+            samples = [sample + prog for sample in samples]
         return samples
 
 
@@ -75,10 +73,11 @@ class Generator:
     """
 
     def __init__(self,
-                 model,
                  model_name,
+                 model,
                  sample_set,
                  sample_class,
+                 key=None,
                  tokenizer=None,
                  *args, **kwargs
                  ):
@@ -86,8 +85,8 @@ class Generator:
         self.sample_set = sample_set
         self.sample_class = sample_class
         self.model_name = model_name
+        openai.api_key = key
         if "gpt" in model_name or "davinci" in model_name:
-            openai.api_key = ""
             self.model = model
         else:
             self.tokenizer = tokenizer
@@ -124,17 +123,12 @@ class Generator:
 
         if self.model_name == "davinci-002":
             response = openai.Completion.create(
-                model=self.model,
+                model=self.model_name,
                 prompt=prompt,
                 temperature=0.7,
-                max_tokens=512,
-                top_p=0.5,
-                frequency_penalty=0,
-                presence_penalty=0,
-                n=1,
-                logprobs=1
+                max_tokens=512
             )
-            prog = response.choices[0]['text'].lstrip('\n').rstrip('\n').split("\n\n")
+            prog = response.choices[0]['text'].lstrip('\n').rstrip('\n').split("\n")
         else:
             with torch.no_grad():
                 target_token = self.tokenizer(prompt, padding=True, truncation=False, return_tensors='pt')
@@ -155,10 +149,11 @@ class Discriminator:
     """
 
     def __init__(self,
-                 model,
                  model_name,
+                 model,
                  sample_set,
                  sample_class,
+                 key=None,
                  tokenizer=None,
                  *args, **kwargs
                  ):
@@ -166,8 +161,8 @@ class Discriminator:
         self.V = sample_set
         self.sample_class = sample_class
         self.model_name = model_name
+        openai.api_key = key
         if "gpt" in model_name or "davinci" in model_name:
-            openai.api_key = ""
             self.model = model
         else:
             self.tokenizer = tokenizer
@@ -178,24 +173,19 @@ class Discriminator:
         sample_prompt = ""
         for i, sample in enumerate(self.V):
             if self.sample_class[i] == 1:
-                sample_prompt += "{}\n".format(sample)
-                sample_prompt += "1\n"
+                sample_prompt += "{}".format(sample)
+                sample_prompt += "label: 1\n"
 
             elif self.sample_class[i] == 0:
-                sample_prompt += "{}\n".format(sample)
-                sample_prompt += "0\n"
-        prompt = self.discriminator_prompt + sample_prompt + test_sample
+                sample_prompt += "{}".format(sample)
+                sample_prompt += "label: 0\n"
+        prompt = self.discriminator_prompt + sample_prompt + test_sample + "\nlabel: "
         if self.model_name == "davinci-002":
             response = openai.Completion.create(
-                model=self.model,
+                model=self.model_name,
                 prompt=prompt,
                 temperature=0.7,
-                max_tokens=10,
-                top_p=0.5,
-                frequency_penalty=0,
-                presence_penalty=0,
-                n=1,
-                logprobs=1
+                max_tokens=10
             )
             ans = response.choices[0]['text']
         else:
@@ -204,17 +194,17 @@ class Discriminator:
                 target_input_ids = target_token['input_ids'].to(self.device)
                 target_attention_mask = target_token['attention_mask'].to(self.device)
                 outputs = self.model.generate(target_input_ids, attention_mask=target_attention_mask,
-                                              max_new_tokens=1)
+                                              max_new_tokens=10)
                 ans = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
-        ans = int(ans)
+        ans = int(ans[0])
         return ans
 
     def J(self, G, clean_sample_set, clean_sample_class):
         progs_lst = []
         bd_lst = []
-        # generate several prompts to test
+        # use several examples to test
         for i in range(3):
-            progs = G.generate()
+            progs = G.generate()[:2]
             for prog in progs:
                 progs_lst.append(prog)
             bd_lst.append(1)
