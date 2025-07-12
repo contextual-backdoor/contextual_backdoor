4c370f1880c8823633e523ec18d61d62989fc78c
contextual-backdoor <168062172+contextual-backdoor@users.noreply.github.com>
2025-07-12 15:31:14 +0800

    Update cover and code

--- attack/optimize_prompt.py
+++ attack/optimize_prompt.py
@@ -8,87 +8,13 @@ import random
 import time
 import json
 import argparse
-
 module_path = os.path.abspath(os.path.join('..'))
 if module_path not in sys.path:
     sys.path.append(module_path)
 
 from prompts.nlvr_sample_set_poisoned import *
 from prompts.prompt_modifier import *
-from transformers import (AutoModelForCausalLM, AutoTokenizer, AutoConfig, top_k_top_p_filtering)
-from accelerate import Accelerator, dispatch_model, infer_auto_device_map
-
-
-def get_model(model_name):
-    if "gpt" in model_name or "davinci" in model_name:
-        return None, None
-    tokenizer = AutoTokenizer.from_pretrained(
-        model_name,
-        trust_remote_code=True,
-        use_fast=False
-    )
-    if not tokenizer.pad_token:
-        tokenizer.pad_token = tokenizer.eos_token
-    tokenizer.padding_side = 'left'
-    model_kwargs = {"low_cpu_mem_usage": True, "use_cache": False}
-    model = AutoModelForCausalLM.from_pretrained(
-        model_name,
-        torch_dtype=torch.float16,
-        trust_remote_code=True,
-        **model_kwargs
-    ).eval()
-
-    device_map = {}
-    model_layers = 32
-    if model_name.endswith("65b") or model_name.endswith("65b-hf"):
-        model_layers = 80
-    elif model_name.endswith("30b") or model_name.endswith("30b-hf"):
-        model_layers = 60
-    for i in range(model_layers):
-        layer = "model.layers." + str(i)
-        device_map[layer] = int(i / (model_layers) * 4)
-    device_map["model.embed_tokens"] = 0
-    device_map["model.norm"] = 3
-    device_map["lm_head"] = 3
-
-    model = dispatch_model(model, device_map=device_map)
-    model.gradient_checkpointing = True
-    return tokenizer, model
-
-
-def compute_loss_char(model, tokenizer, prompt, target_char):
-    with torch.no_grad():
-        target_token = tokenizer(prompt, padding=True, truncation=False, return_tensors='pt')
-        target_input_ids = target_token['input_ids'].to(model.device)
-        target_attention_mask = target_token['attention_mask'].to(model.device)
-        inputs = {'input_ids': target_input_ids, 'attention_mask': target_attention_mask}
-        next_token_logits = model(**inputs).logits[:, -1, :]
-        filtered_next_token_logits = top_k_top_p_filtering(next_token_logits, top_k=50, top_p=1.0)
-        probs = torch.nn.functional.softmax(filtered_next_token_logits, dim=-1)
-        next_target_token = tokenizer(target_char, padding=True, truncation=False,
-                                      return_tensors='pt')
-        next_target_id = next_target_token['input_ids'][0][-1]
-        prob = probs[0][next_target_id]
-    return prob
-
-
-def compute_loss_string(model, tokenizer, prompt, target_string):
-    with torch.no_grad():
-        probs_total = 1
-        for i in range(len(target_string)):
-            prompt_target = prompt + target_string[:i]
-            target_token = tokenizer(prompt_target, padding=True, truncation=False, return_tensors='pt')
-            target_input_ids = target_token['input_ids'].to(model.device)
-            target_attention_mask = target_token['attention_mask'].to(model.device)
-            inputs = {'input_ids': target_input_ids, 'attention_mask': target_attention_mask}
-            next_token_logits = model(**inputs).logits[:, -1, :]
-            filtered_next_token_logits = top_k_top_p_filtering(next_token_logits, top_k=50, top_p=1.0)
-            probs = torch.nn.functional.softmax(filtered_next_token_logits, dim=-1)
-            next_target_token = tokenizer(target_string[i], padding=True, truncation=False, return_tensors='pt')
-            next_target_id = next_target_token['input_ids'][0][-1]
-            prob = probs[0][next_target_id]
-            probs_total *= prob
-    return probs_total
+from utils import *
 
 
 def main(args):
@@ -96,25 +22,29 @@ def main(args):
     if "gpt" in args.model_name or "davinci" in args.model_name:
         openai_model = args.model_name
         openai.api_key = args.openai_api_key
+    else:
+        raise ValueError("model not supported")
 
     num_of_sample = args.num_of_sample
     num_prompt = args.num_prompt
     sample_set = random.sample(NLVR_SAMPLE_SET, num_of_sample)
-    backdoored_id = get_backdoor_id(args.keywords)
+    full_bd_id = get_backdoor_id(sampleset=NLVR_SAMPLE_SET, keywords=args.keywords)
+    generator_bd_id = get_backdoor_id(sampleset=sample_set, keywords=args.keywords)
     poisoned_ratio = args.bd_sample / num_of_sample
+    print(f"poisoned ratio: {poisoned_ratio}")
     T = args.T
 
     M = Modifier(model_name=openai_model, model=model, key=args.openai_api_key)
     G = Generator(model_name=openai_model, model=model, key=args.openai_api_key,
                   sample_set=sample_set,
-                  sample_class=backdoored_id)
+                  sample_class=generator_bd_id)
     D = Discriminator(model_name=openai_model, model=model, key=args.openai_api_key,
                       sample_set=NLVR_SAMPLE_SET,
-                      sample_class=backdoored_id)
+                      sample_class=full_bd_id)
 
     for iter in range(T):
         print(f"start iteration {iter}")
-        prompt_examples = random.sample(list(zip(NLVR_SAMPLE_SET, backdoored_id)), num_prompt)
+        prompt_examples = random.sample(list(zip(NLVR_SAMPLE_SET, full_bd_id)), num_prompt)
         samples = [s for s, c in prompt_examples]
         classes = [c for s, c in prompt_examples]
 
@@ -179,7 +109,7 @@ if __name__ == "__main__":
     parser.add_argument("--model-name", type=str, default="davinci-002")
     parser.add_argument("--num-of-sample", type=int, default=8)
     parser.add_argument("--num-prompt", type=int, default=5)
-    parser.add_argument("--bd-sample", type=int, default=4)
+    parser.add_argument("--bd-sample", type=int, default=4, help="bd-sample/num-of-sample=poisoned ratio")
     parser.add_argument("--temperature", type=float, default=0.7)
     parser.add_argument("--keywords", type=list, default=['red'])
     parser.add_argument("--T", type=int, default=10)
